94d8d9c1871e51a7e150fefc1adf6d88b7b4e48a
metro7 <81774994+metro7@users.noreply.github.com>
2023-09-16 21:09:22 -0400

    Timer
    Added timer that counts up when the game starts.

---
+++ Assets/Scrits/TimerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimerController : MonoBehaviour
+{
+    public float timeRemaining = 0;
+    public bool timeIsRunning = true;
+    public TMP_Text timeText;
+
+   
+
+    void Start()
+    {
+        timeIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timeIsRunning)
+        {
+            if (timeRemaining >= 0)
+            {
+                timeRemaining += Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timeText.text = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+    }
+}
