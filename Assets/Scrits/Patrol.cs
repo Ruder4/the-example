a9e29a15ff860475080eb78515c72d14de9f5338
Ruder4 <ryder.j.anderson2004@gmail.com>
2023-09-21 17:30:17 -0400

    Labyrinth Dweller Update
    added these cool little goblins, some barrels, rats, and tweaked some audio stuff

--- Assets/Scrits/Patrol.cs
+++ Assets/Scrits/Patrol.cs
@@ -7,27 +7,14 @@ public class Patrol : MonoBehaviour
     private int _currentWaypointIndex = 0;
     private float _speed = 35f;
 
-    private float _waitTime = 0f; // in seconds
-    private float _waitCounter = 0f;
-    private bool _waiting = false;
 
     private void Update()
     {
-        if (_waiting)
-        {
-            _waitCounter += Time.deltaTime;
-            if (_waitCounter < _waitTime)
-                return;
-            _waiting = false;
-        }
 
         Transform wp = waypoints[_currentWaypointIndex];
         if (Vector3.Distance(transform.position, wp.position) < 0.01f)
         {
             transform.position = wp.position;
-            _waitCounter = 0f;
-            _waiting = true;
-
             _currentWaypointIndex = (_currentWaypointIndex + 1) % waypoints.Length;
         }
         else
