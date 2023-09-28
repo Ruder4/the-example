dac0c0e7decc9ce53c71f31a2454d66a5d81e7ff
Jakob Olsen <jolsen618@gmail.com>
2023-09-27 22:18:02 -0400

    Revert "Merge branch 'torch-testing' into Testing"
    This reverts commit 3f8587789dac10f3f4cbaad93916c572bdd7cc75.

--- Assets/Scrits/Pickup.cs
+++ Assets/Scrits/Pickup.cs
@@ -6,12 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class Pickup : MonoBehaviour
 {
-    public int pieces;
-
-    [Header("Torch Test Stuff")]
-	public Light playerLantern;
-	public int torchRange = 500;
-    public int torchBurnoutTime = 20;
+  public int pieces;
 
     private void OnCollisionEnter(Collision other)
     {
@@ -23,23 +18,11 @@ public class Pickup : MonoBehaviour
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
             }
-        } else if (other.gameObject.tag == "torch")
-        {
-            StopCoroutine(torchTimer()); // This makes sure the timer for the torch resets if you pick up two before the first one runs out.
-            StartCoroutine(torchTimer()); 
-            Destroy(other.gameObject); // Destroy the torch object (so you can't pick it up again oooo scary)
         }
         else if (pieces <= 4)
         {
             Console.WriteLine("not enuff");
         }
-    }
-
-    IEnumerator torchTimer()
-    {
-		playerLantern.range = torchRange; // Uses the players already-built-in lantern and just increases the range
-		yield return new WaitForSeconds(torchBurnoutTime); // Wait twenty seconds
-        playerLantern.range = 100; // Resets range back to normal.
     }
 }
 
--- Assets/Scrits/ThirdPersonMovement.cs
+++ Assets/Scrits/ThirdPersonMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.ProBuilder.MeshOperations;
 
-public class PlayerMovement : MonoBehaviour
+public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
