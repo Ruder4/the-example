9c5137eef982c1ba2562d8f3abd51e03a555bb7b
Jakob Olsen <jolsen618@gmail.com>
2023-09-27 22:39:28 -0400

    puzzle works, scene index has an issue (cannot get to kill screen)

--- Assets/Scrits/Pickup.cs
+++ Assets/Scrits/Pickup.cs
@@ -1,45 +1,69 @@
+
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Pickup : MonoBehaviour
 {
-    public int pieces;
+	public int pieces;
 
-    [Header("Torch Test Stuff")]
+	[Header("Puzzle Piece UI Update")]
+	public GameObject puzzlePieceUI;
+
+	[Header("Torch Test Stuff")]
 	public Light playerLantern;
 	public int torchRange = 500;
-    public int torchBurnoutTime = 20;
+	public int torchBurnoutTime = 20;
 
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.tag == "puzzle")
-        {
-            pieces = pieces + 1;
-            other.gameObject.SetActive(false);
-            if (pieces == 5)
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
-            }
-        } else if (other.gameObject.tag == "torch")
-        {
-            StopCoroutine(torchTimer()); // This makes sure the timer for the torch resets if you pick up two before the first one runs out.
-            StartCoroutine(torchTimer()); 
-            Destroy(other.gameObject); // Destroy the torch object (so you can't pick it up again oooo scary)
-        }
-        else if (pieces <= 4)
-        {
-            Console.WriteLine("not enuff");
-        }
-    }
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "puzzle")
+		{
+			pieces = pieces + 1;
+			ChangePuzzleUI(pieces);
+			other.gameObject.SetActive(false);
+			if (pieces == 5)
+			{
+				SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+			}
+			else
+			{
+				Debug.Log("Not enough.");
+			}
+		}
+		else if (other.gameObject.tag == "torch")
+		{
+			StopCoroutine(torchTimer()); // This makes sure the timer for the torch resets if you pick up two before the first one runs out.
+			StartCoroutine(torchTimer());
+			Destroy(other.gameObject); // Destroy the torch object (so you can't pick it up again oooo scary)
+		}
+	}
 
-    IEnumerator torchTimer()
-    {
+	IEnumerator torchTimer()
+	{
 		playerLantern.range = torchRange; // Uses the players already-built-in lantern and just increases the range
 		yield return new WaitForSeconds(torchBurnoutTime); // Wait twenty seconds
-        playerLantern.range = 100; // Resets range back to normal.
-    }
+		playerLantern.range = 100; // Resets range back to normal.
+	}
+
+	void ChangePuzzleUI(int pieceNumber)
+	{
+		Sprite puzzleSprite = Resources.Load<Sprite>($"pp{pieceNumber} ss");
+		RuntimeAnimatorController puzzleController = Resources.Load<RuntimeAnimatorController>($"pp{pieceNumber} ss_0");
+
+		Image puzzleImage = puzzlePieceUI.GetComponent<Image>();
+		Animator puzzleAnimator = puzzlePieceUI.GetComponent<Animator>();
+
+		Color color = puzzleImage.color;
+		color.a = 1f;
+		puzzleImage.color = color;
+
+		puzzleImage.sprite = puzzleSprite;
+		puzzleAnimator.runtimeAnimatorController = puzzleController;
+	}
 }
 
