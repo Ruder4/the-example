d9259bd9b7395f9a63b7a4561ec5dddd92c84428
Jakob Olsen <jolsen618@gmail.com>
2023-09-28 23:22:13 -0400

    fixed bug that caused player to still be paused after returning to menu

--- Assets/Scrits/PauseMenu.cs
+++ Assets/Scrits/PauseMenu.cs
@@ -62,6 +62,7 @@ public class PauseMenu : MonoBehaviour
 
     public void QuitGame()
 	{
-        SceneManager.LoadScene("Menu");
+		Time.timeScale = 1.0f;
+		SceneManager.LoadScene("Menu");
 	}
 }
